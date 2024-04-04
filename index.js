1bfa13fe92779ce93373e48e0b763fabec11d42a
zahaanshapoorjee <zahaan@shapoorjee.com>
2024-04-04 17:16:48 +0530

    new firebase reset

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ import { Console } from 'console';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const serviceAccount = JSON.parse(fs.readFileSync(join(__dirname, 'firebase-credentials.json'), 'utf8'));
+const serviceAccount = JSON.parse(fs.readFileSync(join(__dirname, 'newcreds.json'), 'utf8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
